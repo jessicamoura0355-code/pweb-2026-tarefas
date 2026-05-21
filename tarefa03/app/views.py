3c989a6994e5e3b48017c3b91a4d30163bfda379
Ana Jéssica <jessicamoura@gmail.com>
2026-05-21 15:54:31 -0300

    tarefa03final

--- tarefa03/app/views.py
+++ tarefa03/app/views.py
@@ -1,7 +1,45 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, "index.html")
+    return render(request, "app/index.html")
 
 def usuarios(request):
-    return render(request, "usuarios.html")
+
+    lista_usuarios = [
+        {
+        "nome": "Bruno",
+        "matricula": 2024189128,
+        "idade": 18,
+        "cidade": "São Tomé"
+        },
+        {
+        "nome": "Emilly",
+        "matricula": 2024181238,
+        "idade": 17,
+        "cidade": "Santa Maria"
+        },
+        {
+        "nome": "Nathalia",
+        "matricula": 2024198128,
+        "idade": 17,
+        "cidade": "Santa Maria"
+        },
+        {
+        "nome": "Marquinhos",
+        "matricula": 2024859828,
+        "idade": 17,
+        "cidade": "São Tomé"
+        },
+        {
+        "nome": "José Luís",
+        "matricula": 202418348,
+        "idade": 18,
+        "cidade": "Sitio Novo"
+        },
+    ]
+
+    context = {
+        "usuarios": lista_usuarios,
+    }
+
+    return render(request, "app/usuarios.html", context)
--- tarefa03/config/urls.py
+++ tarefa03/config/urls.py
@@ -18,7 +18,9 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include(app.urls)),
+    path("", include("app.urls")),
+    path("usuarios/", include("app.urls")),
     path('admin/', admin.site.urls),
+
 ]
-]
+
